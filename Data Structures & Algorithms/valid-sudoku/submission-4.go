f07bef11423db74a8951ae5b2a14d1071021222c
Tuguldur (Theo) <dl.tuguldur@gmail.com>
2026-08-18 22:22:06 -0500

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.go
@@ -0,0 +1,50 @@
+func isValidBox(board [][]byte, _row int, _col int) bool {
+	hashset := make(map[byte]bool)
+	for row := _row; row < _row+3; row++ {
+		for col := _col; col < _col+3; col++ {
+			if board[row][col] == '.' {
+				continue
+			}
+			if hashset[board[row][col]] {
+				return false
+			}
+			hashset[board[row][col]]=true
+		}
+	} 
+	return true
+}
+func isValidSudoku(board [][]byte) bool {
+	n := len(board)
+	for row := 0; row < n; row+=3 {
+		for col := 0; col < n; col+=3 {
+			if !isValidBox(board, row, col) {
+				return false
+			}
+		}
+	}
+	for row := 0; row < n; row++ {
+		hashset := make(map[byte]bool)
+		for col := 0; col < n; col++ {
+			if board[row][col] == '.' {
+				continue
+			}
+			if hashset[board[row][col]]  {
+				return false
+			}
+			hashset[board[row][col]]=true
+		}
+	}
+	for col := 0; col < n; col++ {
+		hashset := make(map[byte]bool)
+		for row := 0; row < n; row++ {
+			if board[row][col] == '.' {
+				continue
+			}
+			if hashset[board[row][col]]  {
+				return false
+			}
+			hashset[board[row][col]]=true
+		}
+	}
+	return true
+}
