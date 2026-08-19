6571a1da7e67707fac6cbb14e890e0337f95f921
Tuguldur (Theo) <dl.tuguldur@gmail.com>
2026-08-18 22:16:00 -0500

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.go
@@ -0,0 +1,57 @@
+func isDuplicated(hashmap map[byte]bool, board [][]byte, row int, col int) bool {
+    if _, ok := hashmap[board[row][col]]; ok {
+        return true
+    }
+    return false
+}
+
+func isInValid(board [][]byte, _row int, _col int) bool {
+	hashset := make(map[byte]bool)
+	for row := _row; row < _row+3; row++ {
+		for col := _col; col < _col+3; col++ {
+			if board[row][col] == '.' {
+				continue
+			}
+			if isDuplicated(hashset, board, row, col) {
+				return false
+			}
+			hashset[board[row][col]]=true
+		}
+	} 
+	return true
+}
+func isValidSudoku(board [][]byte) bool {
+	n := len(board)
+	for row := 0; row < n; row+=3 {
+		for col := 0; col < n; col+=3 {
+			if !isInValid(board, row, col) {
+				return false
+			}
+		}
+	}
+	for row := 0; row < n; row++ {
+		hashset := make(map[byte]bool)
+		for col := 0; col < n; col++ {
+			if board[row][col] == '.' {
+				continue
+			}
+			if isDuplicated(hashset, board, row, col) {
+				return false
+			}
+			hashset[board[row][col]]=true
+		}
+	}
+	for col := 0; col < n; col++ {
+		hashset := make(map[byte]bool)
+		for row := 0; row < n; row++ {
+			if board[row][col] == '.' {
+				continue
+			}
+			if isDuplicated(hashset, board, row, col) {
+				return false
+			}
+			hashset[board[row][col]]=true
+		}
+	}
+	return true
+}
