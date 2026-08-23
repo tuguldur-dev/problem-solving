ca43c8db051755bc304ac97a90ef8111116e354b
Tuguldur (Theo) <dl.tuguldur@gmail.com>
2026-08-22 23:33:51 -0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.go
@@ -0,0 +1,16 @@
+func search(nums []int, target int) int {
+	left := 0
+	right := len(nums)-1
+
+	for left <= right {
+		middle := (right+left) / 2
+		if nums[middle] < target {
+			left = middle + 1
+		} else if nums[middle] > target {
+			right = middle - 1
+		} else {
+			return middle
+		}
+	}
+	return -1
+}
